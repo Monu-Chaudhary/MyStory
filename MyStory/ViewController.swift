cf853ed36beb5b0f6428accb79672931e8f376d3
Monu-Chaudhary <chaudharymonu45@gmail.com>
2023-02-26 17:21:43 -0500

    autolayout and details complete

--- MyStory/ViewController.swift
+++ MyStory/ViewController.swift
@@ -9,9 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let about = ShivaShakti(name: "Shiva Shakti", image: UIImage(named: "moon")!, location: "Himalayas", title: "Who am I?", description: "I am a devine power who live on Himalayas. I am also known as Mahadev - God of Gods. I live a ascetic life away from all the materialistic pleasures. I wear tiger skin around my waist. I have snake rapped around my neck and Ganga - holy water fountain originating from my hair. I have a damaru on my one hand and a trishul on my other hand. I am the destroyer of evil. My third eye opens and I start dancing Tandav when I am angry. I am also called Bholenath - the kind God because it is easy to please me and I am very much generoug towards my devotees.")
-    let background = ShivaShakti(name: "Shiva Shakti", image: UIImage(named: "moon")!, location: "Himalayas", title: "What is my background?", description: "BG")
-    let powers = ShivaShakti(name: "Shiva Shakti", image: UIImage(named: "moon")!, location: "Himalayas", title: "What are my powers?", description: "POWERS")
+    let about = ShivaShakti(name: "Shiva Shakti", image: UIImage(named: "moon")!, location: "Himalayas", title: "What is Shiva?", description: "I am a devine power who lives on Himalayas.  I wear tiger skin around my waist. I have snake rapped around my neck and Ganga - holy water fountain originating from my hair. I have a damaru on my one hand and a trishul on my other hand.")
+    let background = ShivaShakti(name: "Shiva Shakti", image: UIImage(named: "moon")!, location: "Himalayas", title: "What am I known as?", description: "I am known as Mahadev - God of Gods. I live a ascetic life away from all the materialistic pleasures. I am also called Bholenath - the kind God because it is easy to please me and I am very much generoug towards my devotees.")
+    let shiva = ShivaShakti(name: "Shiva Shakti", image: UIImage(named: "moon")!, location: "Himalayas", title: "What is Shiva?", description: "Shiva is the embodument of consciousness. Shiva is the stillness, the gap between breaths, the emtyness, and all that is formless. Shiva is pure potential where all things can manifest.")
+    let shakti = ShivaShakti(name: "Shiva Shakti", image: UIImage(named: "moon")!, location: "Himalayas", title: "What is Shakti?", description: "Shakti is the power that fuels everything. She is the feminine energy, the fire, and all that manifests.")
 
     
     var contexts: [ShivaShakti] = []
@@ -19,7 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        contexts = [about, background, powers]
+        contexts = [about, background, shiva, shakti]
     }
 
     @IBAction func didClickButton(_ sender: UIButton) {
@@ -38,8 +39,10 @@ class ViewController: UIViewController {
                 detailViewController.context = contexts[1]
             } else if tappedView.tag == 2 {
                 detailViewController.context = contexts[2]
+            } else if tappedView.tag == 3 {
+                detailViewController.context = contexts[3]
             } else {
-                print("no Dinosaur was tapped, please check your selection.")
+                print("no context was clicked, please check your selection.")
             }
         }
     }
